ba2e998ede4ab61ac6c914cbf382e55f65838d1c
Suprim Bhandari <suprimbhandari@mac.mynetworksettings.com>
2025-10-05 19:38:37 -0400

    New file structure

---
+++ __init__.py
@@ -0,0 +1,7 @@
+touch backend/__init__.py \
+      backend/app/__init__.py \
+      backend/app/routers/__init__.py \
+      backend/app/deps/__init__.py \
+      backend/app/services/__init__.py \
+      backend/app/schemas/__init__.py \
+      backend/app/models/__init__.py
